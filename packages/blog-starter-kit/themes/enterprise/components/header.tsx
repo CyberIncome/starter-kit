06dba9ab3121d4c790bb9c013bec47eebb1242f8
CyberIncome <cyberincomeinnovators@gmail.com>
2025-08-26 14:52:59 -0500

    navbar

--- packages/blog-starter-kit/themes/enterprise/components/header.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/header.tsx
@@ -45,7 +45,7 @@ export const Header = () => {
 						href={item.url}
 						target="_blank"
 						rel="noopener noreferrer"
-						className="transition-200 block max-w-[200px] truncate text-ellipsis whitespace-nowrap rounded-full p-2 transition-colors hover:bg-white hover:text-black dark:hover:bg-neutral-800 dark:hover:text-white"
+						className="transition-200 block max-w-[300px] truncate text-ellipsis whitespace-nowrap rounded-full p-2 transition-colors hover:bg-white hover:text-black dark:hover:bg-neutral-800 dark:hover:text-white"
 					>
 						{item.label}
 					</a>
